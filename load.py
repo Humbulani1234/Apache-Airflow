b17b680959c794cf80fcaa962582b94e95785e2f
Humbulani <ndousmall@gmail.com>
2023-08-23 17:52:47 +0200

    make good

--- load.py
+++ load.py
@@ -1,27 +1,14 @@
 
-#sql_tables = "CREATE TABLE securities (Symbol VARCHAR(255),Security VARCHAR(255>
- #VARCHAR(255),GICS_Sub_Industry VARCHAR(255),Headquaters_Location VARCHAR(255),>
-
-#sql_data = "INSERT INTO securities (Symbol, Security, SEC_filings, GICS_Sector,>
- #) VALUES (%s,%s,%s,%s,%s,%s)"
-
-#header_data = ["Symbol", "Security", "SEC_filings", "GICS_Sector", "GICS_Sub_In>
-
-#table_name = "SELECT * FROM securities"
-
 import mysql.connector
-#import new
 from new import df_to_tuple
 
-# =======================================
-# Insert data into and SQL Table function
-#========================================
 
 def INSERT_INTO_TABLE(http_address, table_data):
     
     ''' Insert data into an SQL Table function'''
     
-    mydb_3 = mysql.connector.connect(host = "127.0.0.1", user = "mysql_user", password = "mysql_pass", database = "mysql_db")
+    mydb_3 = mysql.connector.connect(host = "127.0.0.1", user = "mysql_user", 
+                                     password = "mysql_pass", database = "mysql_db") # Create password ENV variables
     mycursor_3 = mydb_3.cursor()
     result = df_to_tuple(http_address)
 
@@ -30,13 +17,4 @@ def INSERT_INTO_TABLE(http_address, table_data):
         mycursor_3.execute(table_data, values)
         mydb_3.commit()
     
-    #mycursor_3.execute("SHOW TABLES")
-    
-    #for x in mycursor_3:
-        #print(x)
-    
-    #print(mycursor_3.rowcount, "was inserted")
-    
     mycursor_3.close()
-    
-#INSERT_INTO_TABLE(http_address, sql_data)
--- new.py
+++ new.py
@@ -1,41 +1,18 @@
-#import datetime as dt
+
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
-#import sys
-#import time
 import mysql.connector
-#import os
-import warnings
-
-# ============
-# Settings
-# ============
+import warning
 
 pd.set_option("display.width", 5000)
 pd.set_option("display.max_columns", 500)
 pd.set_option("display.max_rows", 600)
 warnings.filterwarnings("ignore")
-# ===================
-# Initializing Inputs
-# ===================
 
 http_address = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-
-#sql_tables = "CREATE TABLE securities (Symbol VARCHAR(255),Security VARCHAR(255),SEC_filings VARCHAR(255),GICS_Sector\
- #VARCHAR(255),GICS_Sub_Industry VARCHAR(255),Headquaters_Location VARCHAR(255),id INT AUTO_INCREMENT PRIMARY KEY)"
-
-#sql_data = "INSERT INTO securities (Symbol, Security, SEC_filings, GICS_Sector, GICS_Sub_Industry, Headquaters_Location\
- #) VALUES (%s,%s,%s,%s,%s,%s)"
-
-#header_data = ["Symbol", "Security", "SEC_filings", "GICS_Sector", "GICS_Sub_Industry", "Headquaters_Location"]
-
-#table_name = "SELECT * FROM securities"
-
-# ========================================
-# Extract table and table header functions
-# ========================================
 
 def Extract_Table(http_address):
     
@@ -57,9 +34,6 @@ def Extract_Table(http_address):
     table = soup.find("table")
     
     return table  
-    
-#a = Extract_Table(http_address)
-#print(a)
 
 def Table_Header(http_address):
     
@@ -74,10 +48,6 @@ def Table_Header(http_address):
         header.append(i.text.strip())
     
     return header   
-    
-# ===================================
-# Extract data (table rows) functions
-# ===================================
 
 def Table_Data(http_address):
     
@@ -100,10 +70,6 @@ def Table_Data(http_address):
     data.pop(0)
     
     return data
-    
-# ========================================
-# Create a dictionary/ Dataframe functions
-# ========================================
 
 def r(j):
     
@@ -117,7 +83,6 @@ def r(j):
         path_data.append(item[j])
         
     return path_data
-
 
 def Dataframe(http_address):
     
@@ -136,9 +101,6 @@ def Dataframe(http_address):
     
     return df
 
-#a = Dataframe(http_address)
-#print(a)
-
 def df_to_tuple(http_address):
     
     '''Convert dataframe rows to tuple for ease of readility 
@@ -148,111 +110,3 @@ def df_to_tuple(http_address):
     result = list(records)
                         
     return result
-
-#a = df_to_tuple(http_address)
-#print(a)
-
-# ===============================
-# Create an SQL database function
-# ===============================
-
-#def CREATE_DATABASE():
-    
-    #''' Create and SQL database function'''
-    #cursor.execute("DROP DATABASE IF EXISTS roads")
-    #mydb = mysql.connector.connect(host = "localhost", user = "mysql_user", password = "mysql_pass")
-    #mycursor = mydb.cursor()
-    #mycursor.execute("DROP DATABASE IF EXISTS mysql_db")
-    #mycursor.execute("CREATE DATABASE mysql_db") # Create database
-    #mycursor.execute("SHOW DATABASES") # Show available databases
-
-    #for x in mycursor: 
-        #print(x)
-        
-    #mycursor.close()
-        
-#CREATE_DATABASE()
-
-# ============================
-# Create an SQL Table function
-#=============================
-
-#def CREATE_TABLES(data):
-    
-    #''' Create an SQL Table function'''
-    
-    #mydb_2 = mysql.connector.connect(host = "localhost", user = "mysql_user", password = "mysql_pass", database = "mysql_db")
-    #mycursor_2 = mydb_2.cursor()
-    #mycursor_2.execute("DROP TABLE IF EXISTS securities")
-    #mycursor_2.execute(sql_tables)
-    #mycursor_2.execute("SHOW TABLES")
-
-    #for x in mycursor_2: 
-        #print(x)
-    
-    #mycursor_2.close()
-    
-#CREATE_TABLES(data=sql_tables)
-
-# =======================================
-# Insert data into and SQL Table function
-#========================================
-
-#def INSERT_INTO_TABLE(http_address, table_data):
-    
-    #''' Insert data into an SQL Table function'''
-    
-    #mydb_3 = mysql.connector.connect(host = "127.0.0.1", user = "mysql_user", password = "mysql_pass", database = "mysql_db")
-    #mycursor_3 = mydb_3.cursor()
-    #result = df_to_tuple(http_address)
-
-    #for element in result:
-        #values = tuple(element)
-        #mycursor_3.execute(table_data, values)
-        #mydb_3.commit()
-    
-    #mycursor_3.execute("SHOW TABLES")
-    
-    #for x in mycursor_3:
-        #print(x)
-    
-    #print(mycursor_3.rowcount, "was inserted")
-    
-    #mycursor_3.close()
-    
-#INSERT_INTO_TABLE(http_address, sql_data)
-
-# =============================
-# Print and SQL Table in python
-# =============================
-
-#def Get_sql_table(header_data, table_name):
-    
-    #'''Print and SQL Table in python'''
-    
-    #mydb_4 = mysql.connector.connect(host = "localhost", user = "mysql_user", password = "mysql_pass", database = "mysql_db")
-    #mycursor_4 = mydb_4.cursor()
-    #mycursor_4.execute(table_name)
-    #my_result = mycursor_4.fetchall()
-
-    #return tabulate(my_result, headers = header_data, tablefmt="psql")
-    
-    #mycursor_4.close()      
-
-# ===============
-# Run the script
-# ===============
-
-#print(Get_sql_table(header_data, table_name))
-
-# ==================
-# Total process time
-# ==================
-
-#print("Process time in seconds: " + str((time.time() - start)))
-
-# ====================================================================================================================================
-
-
-
-
